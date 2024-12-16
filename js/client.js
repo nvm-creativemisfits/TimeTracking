2cb4b584a2520301e9f6c6f8b2113ce37a46851a
nvm-creativemisfits <msanchez@newvisualmedia.com>
2024-12-16 12:05:49 -0700

    Add files via upload

---
+++ js/client.js
@@ -0,0 +1,385 @@
+/* global TrelloPowerUp */
+
+// we can access Bluebird Promises as follows
+var Promise = TrelloPowerUp.Promise;
+
+/*
+
+Trello Data Access
+
+The following methods show all allowed fields, you only need to include those you want.
+They all return promises that resolve to an object with the requested fields.
+
+Get information about the current board
+t.board('id', 'name', 'url', 'shortLink', 'members')
+
+Get information about the current list (only available when a specific list is in context)
+So for example available inside 'attachment-sections' or 'card-badges' but not 'show-settings' or 'board-buttons'
+t.list('id', 'name', 'cards')
+
+Get information about all open lists on the current board
+t.lists('id', 'name', 'cards')
+
+Get information about the current card (only available when a specific card is in context)
+So for example available inside 'attachment-sections' or 'card-badges' but not 'show-settings' or 'board-buttons'
+t.card('id', 'name', 'desc', 'due', 'closed', 'cover', 'attachments', 'members', 'labels', 'url', 'shortLink', 'idList')
+
+Get information about all open cards on the current board
+t.cards('id', 'name', 'desc', 'due', 'closed', 'cover', 'attachments', 'members', 'labels', 'url', 'shortLink', 'idList')
+
+Get information about the current active Trello member
+t.member('id', 'fullName', 'username')
+
+For access to the rest of Trello's data, you'll need to use the RESTful API. This will require you to ask the
+user to authorize your Power-Up to access Trello on their behalf. We've included an example of how to
+do this in the `🔑 Authorization Capabilities 🗝` section at the bottom.
+
+*/
+
+/*
+
+Storing/Retrieving Your Own Data
+
+Your Power-Up is afforded 4096 chars of space per scope/visibility
+The following methods return Promises.
+
+Storing data follows the format: t.set('scope', 'visibility', 'key', 'value')
+With the scopes, you can only store data at the 'card' scope when a card is in scope
+So for example in the context of 'card-badges' or 'attachment-sections', but not 'board-badges' or 'show-settings'
+Also keep in mind storing at the 'organization' scope will only work if the active user is a member of the team
+
+Information that is private to the current user, such as tokens should be stored using 'private' at the 'member' scope
+
+t.set('organization', 'private', 'key', 'value');
+t.set('board', 'private', 'key', 'value');
+t.set('card', 'private', 'key', 'value');
+t.set('member', 'private', 'key', 'value');
+
+Information that should be available to all users of the Power-Up should be stored as 'shared'
+
+t.set('organization', 'shared', 'key', 'value');
+t.set('board', 'shared', 'key', 'value');
+t.set('card', 'shared', 'key', 'value');
+t.set('member', 'shared', 'key', 'value');
+
+If you want to set multiple keys at once you can do that like so
+
+t.set('board', 'shared', { key: value, extra: extraValue });
+
+Reading back your data is as simple as
+
+t.get('organization', 'shared', 'key');
+
+Or want all in scope data at once?
+
+t.getAll();
+
+*/
+
+var GLITCH_ICON = './images/glitch.svg';
+var WHITE_ICON = './images/icon-white.svg';
+var GRAY_ICON = './images/icon-gray.svg';
+
+var randomBadgeColor = function() {
+  return ['green', 'yellow', 'red', 'none'][Math.floor(Math.random() * 4)];
+};
+
+var getBadges = function(t){
+  return t.card('name')
+  .get('name')
+  .then(function(cardName){
+    console.log('We just loaded the card name for fun: ' + cardName);
+    
+    return [{
+      // dynamic badges can have their function rerun after a set number
+      // of seconds defined by refresh. Minimum of 10 seconds.
+      dynamic: function(){
+        // we could also return a Promise that resolves to this as well if we needed to do something async first
+        return {
+          title: 'Detail Badge', // for detail badges only
+          text: 'Dynamic ' + (Math.random() * 100).toFixed(0).toString(),
+          icon: GRAY_ICON, // for card front badges only
+          color: randomBadgeColor(),
+          refresh: 10 // in seconds
+        };
+      }
+    }, {
+      // its best to use static badges unless you need your badges to refresh
+      // you can mix and match between static and dynamic
+      title: 'Detail Badge', // for detail badges only
+      text: 'Static',
+      icon: GRAY_ICON, // for card front badges only
+      color: null
+    }, {
+      // card detail badges (those that appear on the back of cards)
+      // also support callback functions so that you can open for example
+      // open a popup on click
+      title: 'Popup Detail Badge', // for detail badges only
+      text: 'Popup',
+      icon: GRAY_ICON, // for card front badges only
+      callback: function(context) { // function to run on click
+        return context.popup({
+          title: 'Card Detail Badge Popup',
+          url: './settings.html',
+          height: 184 // we can always resize later, but if we know the size in advance, its good to tell Trello
+        });
+      }
+    }, {
+      // or for simpler use cases you can also provide a url
+      // when the user clicks on the card detail badge they will
+      // go to a new tab at that url
+      title: 'URL Detail Badge', // for detail badges only
+      text: 'URL',
+      icon: GRAY_ICON, // for card front badges only
+      url: 'https://trello.com/home',
+      target: 'Trello Landing Page' // optional target for above url
+    }];
+  });
+};
+
+var boardButtonCallback = function(t){
+  return t.popup({
+    title: 'Popup List Example',
+    items: [
+      {
+        text: 'Open Modal',
+        callback: function(t){
+          return t.modal({            
+            url: './modal.html', // The URL to load for the iframe
+            args: { text: 'Hello' }, // Optional args to access later with t.arg('text') on './modal.html'
+            accentColor: '#F2D600', // Optional color for the modal header 
+            height: 500, // Initial height for iframe; not used if fullscreen is true
+            fullscreen: true, // Whether the modal should stretch to take up the whole screen
+            callback: () => console.log('Goodbye.'), // optional function called if user closes modal (via `X` or escape)
+            title: 'Hello, Modal!', // Optional title for modal header
+            // You can add up to 3 action buttons on the modal header - max 1 on the right side.
+            actions: [{
+              icon: GRAY_ICON,
+              url: 'https://google.com', // Opens the URL passed to it.
+              alt: 'Leftmost',
+              position: 'left',
+            }, {
+              icon: GRAY_ICON,
+              callback: (tr) => tr.popup({ // Callback to be called when user clicks the action button.
+                title: 'Settings',
+                url: 'settings.html',
+                height: 164,
+              }),
+              alt: 'Second from left',
+              position: 'left',
+            }, {
+              icon: GRAY_ICON,
+              callback: () => console.log('🏎'),
+              alt: 'Right side',
+              position: 'right',
+            }],
+          })
+        }
+      },
+      {
+        text: 'Open Board Bar',
+        callback: function(t){
+          return t.boardBar({
+            url: './board-bar.html',
+            height: 200
+          })
+          .then(function(){
+            return t.closePopup();
+          });
+        }
+      }
+    ]
+  });
+};
+
+var cardButtonCallback = function(t){
+  // Trello Power-Up Popups are actually pretty powerful
+  // Searching is a pretty common use case, so why reinvent the wheel
+  var items = ['acad', 'arch', 'badl', 'crla', 'grca', 'yell', 'yose'].map(function(parkCode){
+    var urlForCode = 'http://www.nps.gov/' + parkCode + '/';
+    var nameForCode = '🏞 ' + parkCode.toUpperCase();
+    return {
+      text: nameForCode,
+      url: urlForCode,
+      callback: function(t){
+        // In this case we want to attach that park to the card as an attachment
+        // but first let's ensure that the user can write on this model
+        if (t.memberCanWriteToModel('card')){
+          return t.attach({ url: urlForCode, name: nameForCode })
+          .then(function(){
+            // once that has completed we should tidy up and close the popup
+            return t.closePopup();
+          });
+        } else {
+          console.log("Oh no! You don't have permission to add attachments to this card.")
+          return t.closePopup(); // We're just going to close the popup for now.
+        };
+      }
+    };
+  });
+
+  // we could provide a standard iframe popup, but in this case we
+  // will let Trello do the heavy lifting
+  return t.popup({
+    title: 'Popup Search Example',
+    items: items, // Trello will search client-side based on the text property of the items
+    search: {
+      count: 5, // How many items to display at a time
+      placeholder: 'Search National Parks',
+      empty: 'No parks found'
+    }
+  });
+  
+  // in the above case we let Trello do the searching client side
+  // but what if we don't have all the information up front?
+  // no worries, instead of giving Trello an array of `items` you can give it a function instead
+  /*
+  return t.popup({
+    title: 'Popup Async Search',
+    items: function(t, options) {
+      // use options.search which is the search text entered so far
+      // and return a Promise that resolves to an array of items
+      // similar to the items you provided in the client side version above
+    },
+    search: {
+      placeholder: 'Start typing your search',
+      empty: 'Huh, nothing there',
+      searching: 'Scouring the internet...'
+    }
+  });
+  */
+};
+
+import { ENV } from './env.js';
+
+// We need to call initialize to get all of our capability handles set up and registered with Trello
+TrelloPowerUp.initialize({
+'card-buttons': function (t, options) {
+    return [
+      {
+        icon: 'https://newvisualmedia.com/wp-content/uploads/2024/12/hour-glass-login.png', // Replace with your Log In icon URL
+        text: 'Log In',
+        callback: function (t) {
+          return t.card('id', 'name').then(function (card) {
+            const cardId = card.id;
+            return t.member('username').then(function (member) {
+              const username = member.username;
+
+              // Call the function to add the login comment
+              postComment(cardId, username, 'logged in', t);
+            });
+          });
+        },
+      },
+      {
+        icon: 'https://newvisualmedia.com/wp-content/uploads/2024/12/hour-glass-logout.png', // Replace with your Log Out icon URL
+        text: 'Log Out',
+        callback: function (t) {
+          return t.card('id', 'name').then(function (card) {
+            const cardId = card.id;
+            return t.member('username').then(function (member) {
+              const username = member.username;
+
+              // Call the function to add the logout comment
+              postComment(cardId, username, 'logged out', t);
+            });
+          });
+        },
+      },
+    ];
+  },
+});
+
+// Function to Post Log In or Log Out Comment
+async function postComment(cardId, username, action, t) {
+  const TRELLO_API_KEY = ENV.TRELLO_API_KEY;
+  const TRELLO_TOKEN = ENV.TRELLO_TOKEN;
+
+  const now = new Date();
+  const timeZoneOffset = -7; // Adjust for your desired time zone (e.g., UTC-5 for EST)
+  const adjustedTime = new Date(now.getTime() + timeZoneOffset * 3600000);
+
+  const formattedTime = adjustedTime.toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
+
+  const formattedDate = adjustedTime.toLocaleDateString('en-US', {
+    weekday: 'long',
+    month: 'numeric',
+    day: 'numeric',
+    year: 'numeric',
+  });
+
+  const timestamp = `${formattedTime} - ${formattedDate}`;
+
+  // Fetch card and board details
+  const cardResponse = await fetch(
+    `https://api.trello.com/1/cards/${cardId}?fields=name,url,idBoard&key=${TRELLO_API_KEY}&token=${TRELLO_TOKEN}`
+  );
+  const cardData = await cardResponse.json();
+  const cardName = cardData.name;
+  const cardUrl = cardData.url;
+
+  // Fetch the board name
+  const boardResponse = await fetch(
+    `https://api.trello.com/1/boards/${cardData.idBoard}?fields=name&key=${TRELLO_API_KEY}&token=${TRELLO_TOKEN}`
+  );
+  const boardData = await boardResponse.json();
+  const boardName = boardData.name;
+
+  // Add comment to the card
+  const commentText = `${username} ${action} at [${timestamp}]`;
+  const commentResponse = await fetch(
+    `https://api.trello.com/1/cards/${cardId}/actions/comments?key=${TRELLO_API_KEY}&token=${TRELLO_TOKEN}`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ text: commentText }),
+    }
+  );
+
+  if (commentResponse.ok) {
+    console.log(`${action} comment added successfully!`);
+
+    // Send data to Google Sheets
+    postToGoogleSheet(username, action, timestamp, boardName, cardName, cardUrl);
+  } else {
+    console.error(`Failed to add ${action} comment. Response status:`, commentResponse.status);
+  }
+}
+
+// Function to post data to Google Sheets
+async function postToGoogleSheet(username, action, timestamp, boardName, cardName, cardUrl) {
+  const googleSheetWebhookURL = ENV.GOOGLE_SHEETS_WEBHOOK_URL;
+
+  const payload = {
+    username: username,
+    action: action,
+    timestamp: timestamp,
+    boardName: boardName,
+    cardName: cardName,
+    cardUrl: cardUrl,
+  };
+
+  try {
+    const response = await fetch(googleSheetWebhookURL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    });
+
+    if (response.ok) {
+      console.log(`Data successfully sent to Google Sheets for ${action}`);
+    } else {
+      console.error(`Failed to send data to Google Sheets: ${response.status}`);
+    }
+  } catch (error) {
+    console.error('Error posting to Google Sheets:', error);
+  }
+}
