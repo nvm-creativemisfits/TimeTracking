549800ab3ac4d2d231edac4579c9046d64da8d51
nvm-creativemisfits <msanchez@newvisualmedia.com>
2024-12-17 14:29:20 -0700

    Update client.js

--- js/client.js
+++ js/client.js
@@ -10,37 +10,33 @@ TrelloPowerUp.initialize({
     return [
       {
         text: 'Log Time',
-        icon: 'https://your-icon-url.com/icon.png',
+        icon: 'https://newvisualmedia.com/wp-content/uploads/2024/12/hour-glass-login.png',
         callback: function (t) {
-          return t.card('id', 'name').then(function (card) {
-            return fetch('https://your-render-app.onrender.com/trello/comment', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                cardId: card.id,
-                comment: `Time Log added for card: ${card.name}`,
-              }),
-            })
-              .then((response) => response.json())
-              .then((data) => {
-                console.log('Comment added successfully:', data);
-                t.alert({
-                  message: 'Time log successfully added!',
-                  duration: 5,
-                });
-              })
-              .catch((error) => {
-                console.error('Error adding comment:', error);
-                t.alert({
-                  message: 'Failed to add time log.',
-                  duration: 5,
-                });
-              });
-          });
-        },
+  return t.card('id', 'name').then(function (card) {
+    return fetch('https://timetracking-auxd.onrender.com/trello/comment', { // Updated URL here
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
       },
-    ];
-  },
-});
+      body: JSON.stringify({
+        cardId: card.id,
+        comment: `Time Log added for card: ${card.name}`,
+      }),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Comment added successfully:', data);
+      t.alert({
+        message: 'Time log successfully added!',
+        duration: 5,
+      });
+    })
+    .catch((error) => {
+      console.error('Error adding comment:', error);
+      t.alert({
+        message: 'Failed to add time log.',
+        duration: 5,
+      });
+    });
+  });
+},
