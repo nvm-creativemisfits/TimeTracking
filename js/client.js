b9b7f6ccb7edde8e82caa1270f921fe4d325d055
nvm-creativemisfits <msanchez@newvisualmedia.com>
2024-12-17 13:28:00 -0700

    Update client.js

--- js/client.js
+++ js/client.js
@@ -1,7 +1,6 @@
 const TrelloPowerUp = window.TrelloPowerUp;
 
 console.log("Client.js loaded successfully!");
-console.log("Backend Server URL: https://timetracking-auxd.onrender.com");
 
 // Initialize Trello Power-Up client
 window.TrelloPowerUp.initialize({
@@ -9,25 +8,27 @@ window.TrelloPowerUp.initialize({
     return [
       {
         icon: "https://newvisualmedia.com/wp-content/uploads/2024/12/hour-glass-login.png",
-        text: "Add Comment",
+        text: "Log Time", // Button Text
         callback: function (t) {
           return t.card("id", "name").then(function (card) {
-            fetch("https://timetracking-auxd.onrender.com/trello/comment", {
+            console.log("Adding comment to card:", card.name);
+
+            return fetch("https://timetracking-auxd.onrender.com/trello/comment", {
               method: "POST",
               headers: { "Content-Type": "application/json" },
               body: JSON.stringify({
                 cardId: card.id,
-                comment: `Comment added from Time Logger: ${card.name}`,
+                comment: `Time Log added for card: ${card.name}`,
               }),
             })
               .then((response) => response.json())
               .then((data) => {
                 console.log("Comment added successfully:", data);
-                alert("Comment added successfully!");
+                alert("Time log successfully added!");
               })
               .catch((error) => {
-                console.error("Error adding comment:", error);
-                alert("Failed to add comment.");
+                console.error("Failed to add comment:", error);
+                alert("Failed to add time log.");
               });
           });
         },
@@ -35,3 +36,4 @@ window.TrelloPowerUp.initialize({
     ];
   },
 });
+
