7fb412b5455058d175ea234a1e0338cc09eb98c0
nvm-creativemisfits <msanchez@newvisualmedia.com>
2024-12-17 13:35:06 -0700

    Update client.js

--- js/client.js
+++ js/client.js
@@ -7,12 +7,18 @@ window.TrelloPowerUp.initialize({
   "card-buttons": function (t) {
     return [
       {
+        // Text that appears on the button
+        text: "Log Time", 
+
+        // Icon URL (must be a valid URL)
         icon: "https://newvisualmedia.com/wp-content/uploads/2024/12/hour-glass-login.png",
-        text: "Log Time", // Button Text
+
+        // Callback function when button is clicked
         callback: function (t) {
           return t.card("id", "name").then(function (card) {
             console.log("Adding comment to card:", card.name);
 
+            // Call the backend API to add a comment
             return fetch("https://timetracking-auxd.onrender.com/trello/comment", {
               method: "POST",
               headers: { "Content-Type": "application/json" },
@@ -36,4 +42,3 @@ window.TrelloPowerUp.initialize({
     ];
   },
 });
-
